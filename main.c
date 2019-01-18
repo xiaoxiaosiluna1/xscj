66e626195a45d41e07a7cb5b19da6e8e3817b3b8
siluna <2261906492@qq.com>
2019-01-18 11:04:40 +0800

    添加学生成绩

--- main.c
+++ main.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
+struct stru
+{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100];
+};
 
 int main(){
-
-    printf("hello world!");
-
+    
+  
+    int index=0;
+    struct stru arr[100];
+    printf("xueshengchengjiguanlixitong\n");
+    while(1)
+    {
+        printf("1---tianjiaxueshengchengji:\n");
+        printf("2---shanchuxueshengchengji:\n");
+        printf("3---xianshhsuoyouxueshengchengji:\n");
+        printf("4---yewu1:suoyouzongfenbujigedexuesheng(zongfenbuchaoguo180)\n");
+        printf("5---yewu2:meiyikedoubujigedexuesheng\n");
+        printf("6---yewu3:xianshizongfenzuigaodexuesheng(zhegeyewuyoubug,ruguoyouduogerenzongfenzuigao,namesuijixianshiyigejike)\n");
+        printf("7---yewu4:xianshipingjunfenzuigao,qiemeiyoubujigekemudexuesheng\n");
+        printf("8---tuichu\n");
+        printf("qingshuru\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1)
+        {
+            printf("qingshuruxueshengxingming:\n");
+            scanf("%s",arr[index].name);
+            printf("qingshuruxueshengyuwenchengji:\n");
+            int yuwen;
+            scanf("%d",&yuwen);
+            arr[index].yuwen=yuwen;
+            int shuxue;
+            scanf("%d",&shuxue);
+            arr[index].shuxue=shuxue;
+            int yingyu;
+            scanf("%d",&yingyu);
+            arr[index].yingyu=yingyu;
+            index++;
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+    }
     return 0;
 
 }
